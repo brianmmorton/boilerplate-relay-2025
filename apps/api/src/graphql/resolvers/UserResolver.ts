6e7b12f15f318768347ece4a6657d535b8ded71c
Brian Morton <brianmmorton@gmail.com>
2025-10-23 22:42:28 -0700

    Read me

--- apps/api/src/graphql/resolvers/UserResolver.ts
+++ apps/api/src/graphql/resolvers/UserResolver.ts
@@ -1,6 +1,5 @@
-import { Resolver, Query, Mutation, Ctx, Arg } from 'type-graphql'
+import { Resolver, Query, Ctx } from 'type-graphql'
 import { User } from '../entities/User.js'
-import { UpdateUserTaxProfileInput } from '../inputs/TaxInputs.js'
 import type { GraphQLContext } from '../context.js'
 import { prisma } from '../../lib/prisma.js'
 
@@ -38,43 +37,6 @@ export class UserResolver {
       plannedHoldPeriod: user.plannedHoldPeriod ?? undefined,
       intends1031Exchange: user.intends1031Exchange,
       taxProfileUpdatedAt: user.taxProfileUpdatedAt ?? undefined,
-    }
-  }
-
-  @Mutation(() => User)
-  async updateUserTaxProfile(
-    @Ctx() context: GraphQLContext,
-    @Arg('input', () => UpdateUserTaxProfileInput) input: UpdateUserTaxProfileInput
-  ): Promise<User> {
-    if (!context.user) {
-      throw new Error('Not authenticated')
-    }
-
-    const updatedUser = await prisma.user.update({
-      where: { id: context.user.id },
-      data: {
-        ...input,
-        taxProfileUpdatedAt: new Date(),
-      }
-    })
-
-    return {
-      id: updatedUser.id,
-      email: updatedUser.email,
-      createdAt: updatedUser.createdAt,
-      updatedAt: updatedUser.updatedAt,
-      annualGrossIncome: updatedUser.annualGrossIncome ? Number(updatedUser.annualGrossIncome) : undefined,
-      filingStatus: updatedUser.filingStatus ?? undefined,
-      stateOfResidence: updatedUser.stateOfResidence ?? undefined,
-      marginalTaxBracket: updatedUser.marginalTaxBracket ? Number(updatedUser.marginalTaxBracket) : undefined,
-      stateTaxRate: updatedUser.stateTaxRate ? Number(updatedUser.stateTaxRate) : undefined,
-      useStandardDeduction: updatedUser.useStandardDeduction,
-      existingItemizedDeductions: updatedUser.existingItemizedDeductions ? Number(updatedUser.existingItemizedDeductions) : undefined,
-      isRealEstateProfessional: updatedUser.isRealEstateProfessional,
-      otherPassiveIncome: updatedUser.otherPassiveIncome ? Number(updatedUser.otherPassiveIncome) : undefined,
-      plannedHoldPeriod: updatedUser.plannedHoldPeriod ?? undefined,
-      intends1031Exchange: updatedUser.intends1031Exchange,
-      taxProfileUpdatedAt: updatedUser.taxProfileUpdatedAt ?? undefined,
     }
   }
 } 
